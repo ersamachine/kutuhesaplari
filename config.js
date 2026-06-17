1f617e548210424b08797338ac56b11036ba1299
ersamachine <easyboxmachine@gmail.com>
2026-06-17 18:44:56 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+/* Apps Script "Web uygulaması" dağıtım adresi. */
+window.API_URL = "https://script.google.com/macros/s/AKfycbw_fxXrgdcEJdG3fKVJuil7_wPMYMOCYqyTcdAM6Dqkttqskt3jQ7KhPBVGEfUOeGbRBw/exec";
